3d0976f514e57437e8a22a1937c469f1ed7c6f7d
Oleksandr Nemesh <prevter@gmail.com>
2024-05-01 22:09:08 +0300

    fix 2.200 drawPolygon

--- src/MirrorShowHitboxesFix.cpp
+++ src/MirrorShowHitboxesFix.cpp
@@ -8,9 +8,15 @@ using namespace geode::prelude;
 static bool s_insideDebugUpdate = false;
 
 class $modify(cocos2d::CCDrawNode) {
+#if GEODE_COMP_GD_VERSION > 22000
     bool drawPolygon(CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor) {
         return CCDrawNode::drawPolygon(verts, count, fillColor, std::abs(borderWidth), borderColor);
     }
+#else
+	void drawPolygon(CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor) {
+        CCDrawNode::drawPolygon(verts, count, fillColor, std::abs(borderWidth), borderColor);
+    }
+#endif
 };
 
 class $modify(GJBaseGameLayer) {

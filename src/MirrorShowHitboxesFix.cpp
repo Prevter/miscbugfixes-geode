40bbefa694a38d448e4f83b62687874f347b85da
Oleksandr Nemesh <prevter@gmail.com>
2024-05-01 22:18:09 +0300

    newline

--- src/MirrorShowHitboxesFix.cpp
+++ src/MirrorShowHitboxesFix.cpp
@@ -9,6 +9,7 @@
 using namespace geode::prelude;
 
 static bool s_insideDebugUpdate = false;
+
 class $modify(cocos2d::CCDrawNode) {
     bool drawPolygon(CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor) {
         return CCDrawNode::drawPolygon(verts, count, fillColor, std::abs(borderWidth), borderColor);
